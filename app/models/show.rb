20c88a3dbd10dbea692c4e79ca0fc5c28da374e0
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-06 04:00:14 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
   
+  def actors_list
+    self.actors.collect do |actor|
+      "#{actor.first_name} #{actor.last_name}"
+    end
+  end
 end
